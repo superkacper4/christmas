4422ec3a5e46bf3e45f93a9fd5a624e8cd5c9184
Kacper <kucza06@wp.pl>
2020-12-20 02:54:05 +0100

    style PDF and add html2canvas

--- src/views/PDF.jsx
+++ src/views/PDF.jsx
@@ -1,61 +1,30 @@
 import React, { useContext } from "react";
-import { renderToString } from "react-dom/server";
 import jsPDF from "jspdf";
-
+import html2canvas from 'html2canvas';
 import Viewer from './Viewer'
 
 import DataContext from '../context/DataContext'
 
-const styles = {
-    fontFamily: "sans-serif",
-    textAlign: "center"
-};
-const container = {
-    width: '30vw',
-    padding: '10px',
-    margin: '0',
-    border: '2px solid black',
-    'background-color': 'red'
-};
-const title = {
-    width: '100%',
-    textAlign: 'center',
-    color: 'red',
-    backgroundColor: 'red',
-    fontSize: '30px'
-};
-const Prints = () => {
-    const dataContext = useContext(DataContext)
-
-    return (
-        <div style={container}>
-            <h3 style={title}>xd</h3>
-            <h4>General Information</h4>
-        </div>
-    )
-};
 
 const PDF = () => {
     const dataContext = useContext(DataContext)
     const print = (e) => {
-        e.preventDefault()
-        const string = renderToString(
-            <Viewer
-                title={dataContext.contextData.title}
-                intro={dataContext.contextData.intro}
-                main={dataContext.contextData.main}
-                ending={dataContext.contextData.ending}
-                signature={dataContext.contextData.signature}
-            />)
-            ;
-        const pdf = new jsPDF("p", "mm", "a4");
+        e.preventDefault();
+        html2canvas(document.getElementById("viewer"))
+            .then((canvas) => {
+                const imgData = canvas.toDataURL('image/png');
+                const pdf = new jsPDF("p", "mm", "a4");
+                const imgProps = pdf.getImageProperties(imgData);
+                const pdfWidth = pdf.internal.pageSize.getWidth();
+                const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+                pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+                pdf.save('download.pdf');
+            });
 
-        pdf.fromHTML(string);
-        pdf.save("pdf");
     };
 
     return (
-        <div style={styles}>
+        <div>
             <Viewer
                 title={dataContext.contextData.title}
                 intro={dataContext.contextData.intro}
--- src/views/Viewer.jsx
+++ src/views/Viewer.jsx
@@ -1,26 +1,81 @@
 import React from 'react';
+import styled from 'styled-components'
 
-const divStyle = {
-    backgroundColor: 'blue',
-    textAlign: "center"
-}
+const StyledDiv = styled.div`
+    background-color: #e6dcbd;
+    border: solid 3px #c25151;
+    padding: 50px ;
+    width: 60vh;
+    position: relative;
+`;
 
-const h2Style = {
-    fontSize: '15px',
-    color: 'red',
-    textAlign: 'center'
-}
+const StyledGift = styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: #c25151;
+    position: absolute;
+    bottom: 5px;
+    right: 5px;
+
+
+    &::before{
+        content: '';
+        top: 50%;
+        left:0;
+        transform: translateY(-50%);
+        position:absolute;
+        height: 10px;
+        width: 100%;
+        background-color: #c2a01a;
+    }
+
+    &::after{
+        content: '';
+        left: 50%;
+        top:0;
+        transform: translateX(-50%);
+        position:absolute;
+        height: 100%;
+        width: 10px;
+        background-color: #c2a01a;
+    }
+`;
+
+const StyledH2 = styled.h2`
+    padding:0;
+    margin: 5px;
+    font-size: 20px;
+    text-align: center;
+    color: #c25151;
+`;
+
+const StyledH3 = styled.h2`
+    padding:0;
+    margin: 5px;
+    font-size: 15px;
+    text-align: left;
+    color: #9e6b6b;
+`;
+
+const StyledP = styled.p`
+    padding:0;
+    margin: 5px;
+    color: black;
+    text-align: justify;
+
+`;
+
 
 const Viewer = ({ title, intro, main, ending, signature }) => {
     return (
-        <div style={divStyle}>
-            <h2 style={h2Style}>{title}</h2>
-            <h3>{intro}</h3>
-            <p>{main}</p>
-            <p>{ending}</p>
-            <h3>{signature}</h3>
-
-        </div>
+        <StyledDiv id="viewer">
+            <StyledH2>{title}</StyledH2>
+            <StyledH3>{intro}</StyledH3>
+            <StyledP>{main}</StyledP>
+            <StyledP>{ending}</StyledP>
+            <StyledH3>{signature}</StyledH3>
+            <StyledGift />
+        </StyledDiv>
     )
 }
 
